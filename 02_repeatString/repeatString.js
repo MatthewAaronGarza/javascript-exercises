78c4ab9bb179a0139190ff0c245c85ba823659cc
Matthew Garza <59824561+MatthewAaronGarza@users.noreply.github.com>
2023-03-07 13:51:21 -0600

    Complete repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,20 @@
 const repeatString = function(string, numRepeat) {
 
     
+
+    if (numRepeat < 0) {
+        return 'ERROR'
+    }
+    
+    let repeatedString = ''
+    
+    for (let i = 0; i < numRepeat; i++) {
+        repeatedString = repeatedString + string
+    }
+
+    return repeatedString
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
